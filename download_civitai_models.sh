7e3901f1dadb1705cb8113e016c6d0c8b0fa8ac7
irisandaperture <119771052+irisandaperture@users.noreply.github.com>
2023-12-27 20:42:20 -0500

    Update download_civitai_models.sh

--- download_civitai_models.sh
+++ download_civitai_models.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-# Read the list of AIR IDs from air_ids.txt
-while IFS= read -r air_id; do
-  # Split AIR ID into model_id and model_version
-  IFS='@' read -ra parts <<< "$air_id"
-  model_id="${parts[0]}"
-  model_version="${parts[1]}"
+# Define the URL prefix for the civitai.com models
+BASE_URL="https://civitai.com/models/"
 
-  # Construct the URL for downloading based on the AIR ID
-  download_url="https://www.civitai.com/download?model_id=$model_id"
+# Specify the file containing the list of models
+MODEL_LIST_FILE="civit_models.txt"
 
-  # Extract the model name from the URL
-  model_name=$(echo "$download_url" | awk -F'/' '{print $(NF-1)}')
+# Create a directory to store the downloaded models
+mkdir -p civit_models
 
-  # Download the model file using curl with a custom filename
-  curl -OJL "$download_url" -o "${model_name}_${model_id}_v${model_version}.zip"
-done < civitai_AIR.txt
+# Change to the directory where the models will be downloaded
+cd civit_models
+
+# Use wget to download each model listed in the text file
+while IFS= read -r model_info || [ -n "$model_info" ]; do
+    model_url="${BASE_URL}${model_info}"
+
+    # Extract model and version information from the URL
+    model_name=$(echo "$model_info" | cut -d'/' -f3)
+    version=$(echo "$model_info" | grep -oP '(?<=modelVersionId=)\d+')
+
+    # Construct the filename based on model and version
+    filename="${model_name}_${version}.zip"
+
+    # Download the model and save it with the constructed filename
+    wget -O "$filename" "$model_url"
+done < "../$MODEL_LIST_FILE"
+
+# Return to the original directory
+cd ..
 
